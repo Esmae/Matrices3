bdb6538a1947d3592229107963e94bc2e6c07279
Esmae <esmae.woods@diamond.ac.uk>
2017-07-07 12:56:32 +0100

    updating scirpt to be the almost default

--- update-gh.sh
+++ update-gh.sh
@@ -1,22 +1,24 @@
-if [ "$TRAVIS_COMMIT_MESSAGE" != "Travis build pushed to testingUpload" ]; then
-  echo -e "Starting to update testingUpload\n"
+if [ "$TRAVIS_COMMIT_MESSAGE" != "Travis build pushed to Matrices" ]; then
+  echo -e "Starting to update Matrices\n"
 
-  cp -R test8 $HOME
+  cp -R testIJK $HOME
+  cp -R testIKJ $HOME
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=testingUpload https://${TOKEN}@github.com/Esmae/Matrices.git  testingUpload > /dev/null
+  git clone --quiet --branch=testTravis https://${TOKEN}@github.com/Esmae/Matrices.git  testTravis> /dev/null
 
-  cp -Rf $HOME/test8 testingUpload
-  cd testingUpload
-  echo -e "1"
+  cp -Rf $HOME/testIJK testTravis
+  cp -Rf $HOME/testIKJ testTravis
+  cd testTravis
+ 
 
   git add -f .
-  echo -e "2"
-  git commit -m "Travis build pushed to testingUpload"
-  echo -e "3"
-  git push -fq https://${TOKEN}@github.com/Esmae/Matrices.git testingUpload > /dev/null
+
+  git commit -m "Travis build pushed to Matrices"
+
+  git push -fq https://${TOKEN}@github.com/Esmae/Matrices.git testTravis > /dev/null
 
   echo -e "Success? \n"
 fi
