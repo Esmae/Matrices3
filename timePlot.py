a8cd67f8214dcefc9e0aeed7ecb80d191124139c
Esmae <esmae.woods@diamond.ac.uk>
2017-07-11 13:38:46 +0100

    adding plotting features

--- timePlot.py
+++ timePlot.py
@@ -1,3 +1,13 @@
+import numpy as np
+import matplotlib as mpl
+mpl.use("Agg")
+from matplotlib import pyplot as mplot
+import csv 
+
+#declaring the lists
+listScore = []
+listCommit = []
+
 f = open("master/dataWithTime50","a")
 
 
@@ -10,3 +20,27 @@ with open("time50") as g:
 
 f.close()
 g.close()
+
+
+with open("master/dataWithTime50") as csvfile:
+    #reading in the data against commits/time file
+    reader = csv.DictReader(csvfile)
+    i = int(0)
+    for row in reader: 
+        #storing the values in the Score columns in a list
+        listScore.append(row['Score'])
+        listCommit.append(i)
+        i=i+1
+#converting the lists to arrays so can plot
+arrScore = np.asarray(listScore)
+arrCommit = np.asarray(listCommit)
+arrScore = arrScore.astype(np.float)
+#plotting the graph
+
+mplot.plot(arrCommit,arrScore,'k')
+#labeling the graph
+mplot.xlabel('Relative commit number')
+mplot.ylabel('Throughput Score/ op/s')
+
+#saving the plot as a png file
+mplot.savefig(TimePlot.png)
