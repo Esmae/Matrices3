3a82abca9659a45e664ea53e95cb888d10d35046
Esmae <esmae.woods@diamond.ac.uk>
2017-07-12 14:03:21 +0100

    Update timePlot50.py
    normalising

--- timePlot50.py
+++ timePlot50.py
@@ -10,7 +10,7 @@ listCommit = []
 
 #opening the file which holds the data with time
 f = open("master/dataWithTime50","a")
-with open("time50") as g:
+with open("master/normData/time50Norm") as g:
     i = int(1)
     for line in g:
         #skipping writing the first line as this contains the header
